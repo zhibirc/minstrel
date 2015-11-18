313713218a40c27f8e0cbfeee7eb13caf995605b
zhibirc <u0059@gmx.com>
2015-11-18 23:17:34 +0200

    Add trash button for user input; fix styles for that button.

--- assets/js/Controllers/AppController.js
+++ assets/js/Controllers/AppController.js
@@ -4,12 +4,17 @@ define(['typography', 'uglifyjs'], function (typography, UglifyJS) {
 	var doc = document,
 		src = doc.getElementById('src_input'),
 		dst = doc.getElementById('dst_output'),
+		srcTrash = doc.getElementById('src_trash'),
+		dstTrash = doc.getElementById('dst_trash'),
+		app = doc.getElementById('app_cnt'),
 		ast, compressor;
 	
 	var speller = new Speller({ url: 'speller', lang: 'ru', options: Speller.IGNORE_URLS });
 	
-	function spellCheck() {
-		speller.check([src]);
+	function toggleTrashIcon(txtTarget) {
+		txtTarget === 'src' ? (src.value ? srcTrash.classList.remove('__hidden') : srcTrash.classList.add('__hidden'))
+							: (dst.value ? dstTrash.classList.remove('__hidden') : dstTrash.classList.add('__hidden'));
+		
 	}
 	
 	function getEvent(evt) {
@@ -18,6 +23,7 @@ define(['typography', 'uglifyjs'], function (typography, UglifyJS) {
 		switch (target.id) {
 		case 'btn_typography':
 			dst.value = typography.fixText(src.value);
+			toggleTrashIcon('dst');
 			break;
 		case 'btn_uglifyjs':
 			ast = UglifyJS.parse(src.value);
@@ -31,12 +37,29 @@ define(['typography', 'uglifyjs'], function (typography, UglifyJS) {
 			ast.mangle_names();
 			
 			dst.value = ast.print_to_string();
+			toggleTrashIcon('dst');
 			break;
 		case 'btn_speller':
-			spellCheck();
+			speller.check([src]);
 			break;
 		case 'btn_speller_settings':
 			speller.optionsDialog();
+			break;
+		case 'src_input':
+			toggleTrashIcon('src');
+			break;
+		case 'dst_output':
+			toggleTrashIcon('dst');
+			break;
+		case 'src_trash':
+			src.value = '';
+			toggleTrashIcon('src');
+			src.focus();
+			break;
+		case 'dst_trash':
+			dst.value = '';
+			toggleTrashIcon('dst');
+			dst.focus();
 			break;
 		}
 	}
--- assets/js/Views/AppView.js
+++ assets/js/Views/AppView.js
@@ -2,9 +2,22 @@ define(['appController'], function (Controller) {
 	'use strict';
 	
 	var doc = document,
-		navBar = doc.getElementById('main_toolbar');
+		navBar = doc.getElementById('main_toolbar'),
+		app = doc.getElementById('app_cnt');
 		
 	navBar.addEventListener('click', function (evt) {
+		evt.stopPropagation();
+		
+		Controller.getEvent(evt);
+	}, false);
+	
+	app.addEventListener('click', function (evt) {
+		evt.stopPropagation();
+		
+		Controller.getEvent(evt);
+	}, false);
+	
+	app.addEventListener('input', function (evt) {
 		evt.stopPropagation();
 		
 		Controller.getEvent(evt);
