ba4837b76d2e557b09f7eba7bbd01b2603280f73
zhibirc <u0059@gmx.com>
2015-11-25 18:51:07 +0200

    [Add|Fix]::app - Added waitSeconds to prevent timeout or script loading; delete cse module.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 /** CONFIG */
 requirejs.config({
 	baseUrl: 'assets/js',
+	waitSeconds: 0,
 	paths: {
 		appView: 'Views/AppView',
 		appController: 'Controllers/AppController',
@@ -8,18 +9,16 @@ requirejs.config({
 		typography: 'Controllers/modules/typography',
 		uglifyjs: 'Controllers/modules/uglifyjs',
 		search: 'Controllers/modules/search',
-		speller: 'Controllers/modules/spell',
-		cse: 'Controllers/modules/cse'
+		speller: 'Controllers/modules/spell'
 	},
 	shim: {
 		uglifyjs: { exports: 'UglifyJS' },
-		speller: { exports: 'Speller' },
-		cse: { exports: '__gcse' }
+		speller: { exports: 'Speller' }
 	}
 });
 
 /** INITIAL */
-require(['appView', 'appController', 'appModel', 'cse'], function (View, Controller, databaseAPI, __gcse) {
+require(['appView', 'appController', 'appModel'], function (View, Controller, databaseAPI) {
 	'use strict';
 	
 	var db = new databaseAPI;
